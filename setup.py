84fd20892b577d74f77163ca124336b277c02523
Artem Moskalenko <18658628+shamandok@users.noreply.github.com>
2021-10-27 00:01:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
-    name='Python-TBot-calendar',
+    name='pyTBotCalendar',
     packages=['TBot_calendar'],
     version='0.0.1',
     license='MIT',
-    description='Календарь на python для telegram ботов',
+    description='Календарь для telegram бота с поддержкой мультиязычности и возможностью кастомизации.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Artem Moskalenko',
     author_email='shamandok22@gmail.com',
-    url='https://github.com/shamandok/Python-TBot-calendar',
+    url='https://github.com/shamandok/pyTBotCalendar',
     keywords=['calendar', 'telegram', 'bot', 'telegram bot'],
     install_requires=[
         'python-dateutil',
@@ -22,7 +22,8 @@ setup(
         'telethon': ['telethon']
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+	"Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
